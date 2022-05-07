3464bec8a0b1c0a6b1a7803eed2f61c95e48e533
Bruno de Freitas <brunofreitas.ads@gmail.com>
2022-05-07 16:54:21 -0300

    Mobile Continues

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -6,7 +6,13 @@ import { gestureHandlerRootHOC} from 'react-native-gesture-handler';
 
 import { styles } from './styles';
 import { theme } from '../../theme';
+
 import { Options } from '../Options';
+import { Form } from '../Form';
+
+import { feedbackTypes } from '../../utils/feedbackTypes';
+
+export type FeedbackType = keyof typeof feedbackTypes;
 
 function Widget() {
 
@@ -35,7 +41,7 @@ function Widget() {
                 backgroundStyle={styles.modal}
                 handleIndicatorStyle={styles.indicator}
             >
-                <Options />
+                <Form feedbackType='BUG'/>
             </BottomSheet>
         </>
     );
