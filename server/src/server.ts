dca5da94deaba4ce9daaf51b01e6d14aaeed64a4
Bruno de Freitas <brunofreitas.ads@gmail.com>
2022-05-09 11:34:50 -0300

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,9 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3333, () => {
-    console.log('HTTP server running!')
+const PORT = process.env.PORT || 3333
+
+app.listen(PORT, () => {
+    console.log(`HTTP server running! on PORT ${PORT}`)
 })
 
